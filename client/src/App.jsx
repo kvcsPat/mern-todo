f2c500b1ff5360c28f0fbea247383b9a37d45ad9
kvcsPat <smth.patrik@gmail.com>
2024-04-19 14:09:34 +0200

    connected backend to frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("/api/todos");
+      const todos = await res.json();
+
+      setMessage(todos.mssg);
+    }
+    getTodos();
+  }, []);
+
   return (
     <main className="container">
       <h1>Todo App</h1>
+      {message && <p>{message}</p>}
     </main>
   );
 }
