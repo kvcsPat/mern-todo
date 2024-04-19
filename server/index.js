85b5e19cb3c34939fb0315649bdaffe9a34e0bea
kvcsPat <smth.patrik@gmail.com>
2024-04-19 13:56:58 +0200

    created and tested endpoints

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.status(200).json({ mssg: "hello people" });
-});
+const router = require("./routes");
+
+app.use("/api", router);
 
 const port = 5000;
 app.listen(port, () => {
